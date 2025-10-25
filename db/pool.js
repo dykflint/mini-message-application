4711d1f9df598f1013951a15adc3654234bcf608
DykFlint <konstantin.tamoev@gmail.com>
2025-10-25 15:22:30 +0200

    Deploy on railway.app

--- db/pool.js
+++ db/pool.js
@@ -5,10 +5,16 @@ import { Pool } from 'pg';
 
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
+// export default new Pool({
+//   host: process.env.HOST, // or wherever the db is hosted
+//   user: process.env.DB_USER,
+//   database: process.env.DATABASE,
+//   password: process.env.PASSWORD,
+//   port: 5432, // The default port
+// });
 export default new Pool({
-  host: process.env.HOST, // or wherever the db is hosted
-  user: process.env.DB_USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: 5432, // The default port
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Required for Railway
+  },
 });
